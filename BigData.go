6fa8b8b32a9c6e6d831f97908c991415c8290976
zhanatmod <zhanatmod@gmail.com>
2018-04-26 12:04:29 +0600

    файл вычисление факториала с большим значением

--- BigData.go
+++ BigData.go
@@ -1,1 +1,26 @@
-package golang
+package main
+
+
+
+import (
+"fmt"
+	"math/big"
+)
+
+func main() {
+
+	var x int
+	x = 5
+	y := fact(x)
+
+	fmt.Println(y)
+}
+
+func fact (x int) big.Int {
+
+	if x == 1 {
+		return 1
+	}
+	return x * fact(x - 1)
+
+}
