bba32029b1ee7aaac11dab43ddd54886c747563c
zhanatmod <zhanatmod@gmail.com>
2018-05-02 11:30:52 +0600

    learning update files

--- learning/6-0.go
+++ learning/6-0.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//	var x [5]int
 	//	x[4] = 100
@@ -25,6 +27,64 @@ func main() {
 	//fmt.Println(total / float64(len(x)))
 
 	//var x []float64
-	x := make([]float64, 5)
+	//x := make([]float64, 5)
+
+	//x := make([]float64, 5, 10)
+
+	//arr := [5]float64{1,2,3,4,5}
+	//x:=arr[0:5]
+
+	//slice1 := []int{1,2,3}
+	//slice2 := append(slice1, 4, 5)
+	//fmt.Println(slice1, slice2)
+
+	//slice1 := []int{1,2,3,4}
+	//slice2 := make([]int, 2)
+	//copy(slice2, slice1)
+	//fmt.Println(slice1, slice2)
+	//x := make(map[string]int)
+	////var x map[string] int
+	//x["key"] = 10
+	//fmt.Println(x["key"])
+
+//x := make(map[int]int)
+//x[2] = 11
+//x[3] = 13
+//delete(x, 3)
+//fmt.Println(x[3])
+
+//element := make(map[string]string)
+//element["Hi"] = "Hilarry"
+//element["DD"] = "Daddy"
+//	if name, ok := element["Hi"]; ok {
+//		fmt.Println(name, ok)
+//	}
+
+//element := map[string]string{
+//	"H":"Hillary",
+//	"Z":"Zhopa",
+//	}
+
+
+	element := map[string]map[string]string{
+		"H":map[string]string{
+			"name":"Hydrogen",
+			"state":"gas",
+		},
+		"He":map[string]string{
+			"name":"Helium",
+			"state":"gas",
+		},
+		"Zh":map[string]string{
+			"name":"Zhan",
+			"state":"solid",
+		},
+	}
+	if el, ok := element["He"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
 
 }
+
+
+
